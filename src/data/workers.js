6f86329140dc271409e7421f66eca668e868f890
Vishnu P Raj <vshnupraj@gmail.com>
2025-01-19 10:19:01 +0530

    update existing file

--- src/data/workers.js
+++ src/data/workers.js
@@ -5,62 +5,67 @@ const workers = [
       name: "Manu",
       phone: "920 743 9816",
       photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
-      places: ["Medical college", "Parambuzha", "Chuttuveli", "SH Mount"], // List of places
+      places: ["Nagambadam","SH Mount", "Chuttuveli","Chavittuveli","Kumaranellur", "Sankranthi","Chemmarampady",
+        "Doctors garden","Medical college", "Dental college", "ESI", "Ammencherry", "Aarppokara", "Panampalam",
+        "Kudamaloor", "Villunni","Thonnamkuzhi", "Ambadi Junction", "Pulinchuvadu","Poothetta Junction", 
+        "Pullarikunnu MGU", "Parambuzha",  ], // List of places
     },
     {
       id: 2,
       name: "Ivin",
       phone: "828 171 7457",
-      photo: "/images/Ph1.jpeg", // Add the image in the public/images folder
+      photo: "./images/Ph1.jpeg", // Add the image in the public/images folder
       places: ["Puthupally", "Manganam" , "Rubberboard Puthupally", "MMP Vanitha"], // List of places
     },
     {
       id: 3,
       name: "Sanoop",
       phone: "790 923 2478",
-      photo: "/images/Vishnu.jpg", // Add the image in the public/images folder
+      photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
       places: ["Kumarakom", "Kummanam", "Thazhathangadi", "Velur", "Thiruvathukkal", "Karapuzha", "Thekkumgopuram", "Thirunakkara", "Puthanangadi"], // List of places
     },
     {
       id: 4,
       name: "Baiju",
       phone: "984 747 2519",
-      photo: "/images/Vishnu.jpg", // Add the image in the public/images folder
-      places: ["Vadavathoor", "Manarcad", "Kalathipady", "Ayarkunnam", "Madhavanpadi"], // List of places
+      photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
+      places: ["Madhavanpadi","Kalathipady","Vadavathoor", "Manarcad", "Thiruvanchur", "Maalam", "Oravaykkal",
+        "Amayannur","Ayarkunnam" ], // List of places
     },
     {
       id: 5,
       name: "Rahul",
       phone: "949 616 1147",
-      photo: "/images/Vishnu.jpg", // Add the image in the public/images folder
-      places: ["Kodimatha"], // List of places
+      photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
+      places: ["TB Road half","TB ML road","Star Junction half","Pallipurathukavu","Kodimatha",
+        "Before manipuzha signal","TB Junction"], // List of places
     },
     {
       id: 6,
       name: "Biju",
       phone: "989 518 6959",
-      photo: "/images/Vishnu.jpg", // Add the image in the public/images folder
+      photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
       places: ["Kanjirapally", "Mundakayam"], // List of places
     },
     {
       id: 7,
       name: "Sathesh",
       phone: "944 776 6109",
-      photo: "/images/Vishnu.jpg", // Add the image in the public/images folder
+      photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
       places: ["Chingavanam", "Pakkil"], // List of places
     },
     {
       id: 8,
       name: "Moses",
       phone: "994 752 1667",
-      photo: "/images/Vishnu.jpg", // Add the image in the public/images folder
+      photo: "./images/Vishnu.jpg", // Add the image in the public/images folder
       places: ["Colletrate", "Kanjikuzhy", "Muttambalam", "Kollad 1st half"], // List of places
     },
     {
       id: 9,
       name: "No person",
       phone: "--------",
-      photo: "/images/No_Image.jpg", // Add the image in the public/images folder
+      photo: "./images/No_Image.jpg", // Add the image in the public/images folder
       places: ["Pamppady", "Kuropada", "Velloor", "Kothala", "Pallikkathode", "8th Mile"], // List of places
     },
     
